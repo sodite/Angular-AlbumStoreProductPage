d6a57d8e16071bf4ffb44774da8ffa8941c65f35
sodite <47553096+sodite@users.noreply.github.com>
2019-11-08 06:44:35 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -11,7 +11,7 @@ import { Album } from '../album';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo; Album;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
